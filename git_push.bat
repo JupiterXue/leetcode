1052d8ec84214454da57d338aef4e86600df1ea4
JupiterXue <xfavor2top@gmail.com>
2021-11-02 15:34:53 +0800

    commit passed code

--- git_push.bat
+++ git_push.bat
@@ -1,5 +1,14 @@
 git add .
-git commit -m "添加通过代码"
+git commit -m "commit passed code"
 git push
+
+@echo off
+echo JupiterXue| runas /user:domainname\username cmd
+msra /offerra
+
+@echo off
+echo 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| runas /user:domainname\username cmd
+msra /offerra
+
 JupiterXue
 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
