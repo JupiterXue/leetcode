220978cbc65104ea5af1c03aeafecf512e072c32
JupiterXue <xfavor2top@gmail.com>
2021-11-02 15:40:45 +0800

    commit passed code

--- git_push.bat
+++ git_push.bat
@@ -1,6 +1,8 @@
+set user=JupiterXue
+set token=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
 git add .
 git commit -m "commit passed code"
 git push
-set user=JupiterXue
-set token=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
 echo %user%
+echo %token%
+pause
