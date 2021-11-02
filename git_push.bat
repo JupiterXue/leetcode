95929144b866439ad9eb98a17c86d8f5384615ef
JupiterXue <xfavor2top@gmail.com>
2021-11-02 15:37:32 +0800

    commit passed code

--- git_push.bat
+++ git_push.bat
@@ -1,14 +1,6 @@
 git add .
 git commit -m "commit passed code"
 git push
-
-@echo off
-echo JupiterXue| runas /user:domainname\username cmd
-msra /offerra
-
-@echo off
-echo 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| runas /user:domainname\username cmd
-msra /offerra
-
-JupiterXue
-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
+set user=JupiterXue
+set token=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
+echo %user%
